465b45d3e4471f08292e05826c7a83d13faed10d
Anas Azamov <anasazamov55@gmail.com>
2025-04-15 18:46:40 +0500

    fixed

--- product/signals.py
+++ product/signals.py
@@ -1,7 +1,7 @@
 # product/signals.py
 from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
-from product.models import Order, Contact, OrderItem
+from product.models import Order, Contact
 import logging
 from bot.views import updater
 from bot.models import BotAdmin
@@ -11,41 +11,30 @@ bot = updater.bot
 
 logger = logging.getLogger(__name__)
 
-# bot_utils.py yoki shu kabi modulga yozing
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from bot.models import BotAdmin
-from bot.views import updater  # updater.bot orqali botga kirish mumkin
+@receiver(m2m_changed, sender=Order.products.through)
+def order_product_changed(sender, instance, action, **kwargs):
 
-bot = updater.bot
-
-def send_order_telegram_message(order):
-    # Bot adminlarni filtrlaymiz
-    bot_admins = BotAdmin.objects.filter(is_active=True)
-    # Orderdagi barcha OrderItem obyektlari orqali mahsulotlar va miqdorlarni olish
-    product_names = ", ".join(
-        [f"{item.product.name} x {item.quantity}" for item in order.order_items.all()]
-    )
-    order_id = order.pk
-    region = order.get_region_display()
-    keyboard = [[
-        InlineKeyboardButton("📦 Посмотреть заказ", callback_data=f"order_{order_id}"),
-    ]]
-    message = (
-        f"🛒 <b>Новый заказ!</b>\n"
-        f"📌 <b>Товар:</b> {product_names}\n"
-        f"📍 <b>Регион:</b> {region}\n"
-        f"🔍 Нажмите, чтобы посмотреть детали заказа."
-    )
-    # Barcha bot adminlarga xabar yuborish
-    for admin in bot_admins:
-        bot.send_message(
-            chat_id=admin.chat_id,
-            text=message,
-            reply_markup=InlineKeyboardMarkup(keyboard),
-            parse_mode="HTML"
+    if action == "post_add":
+        bot_admins = BotAdmin.objects.filter(is_active=True)
+        # product_names = ", ".join([p.name for p in instance.product.all()])
+        order_id = instance.pk
+        region = instance.get_region_display()
+        keyboard = [[
+            InlineKeyboardButton("📦 Посмотреть заказ", callback_data=f"order_{order_id}"),
+        ]]
+        message = (
+            f"🛒 <b>Новый заказ!</b>\n"
+            # f"📌 <b>Товар:</b> {product_names}\n"
+            f"📍 <b>Регион:</b> {region}\n"
+            f"🔍 Нажмите, чтобы посмотреть детали заказа."
         )
-
-
+        for admin in bot_admins:
+            bot.send_message(
+                chat_id=admin.chat_id,
+                text=message,
+                reply_markup=InlineKeyboardMarkup(keyboard),
+                parse_mode="HTML"
+            )
 
 @receiver(post_save, sender=Contact)
 def contact_created_signal(sender, instance, created, **kwargs):
